ba2a8d84b9511889ced28aef5cc93187160d6cd3
gustavo <gustavo.f.tarano@gmail.com>
2025-01-15 18:17:08 -0300

    Final da parte 2 do front-end "tela principal"

--- js/script.js
+++ js/script.js
@@ -57,45 +57,132 @@ document.querySelector('.home').onmouseleave = (e) =>{
     })
  }
 
- let wrapper = document.querySelector('.wrapper-box');
- let activeBox = wrapper.querySelectorAll('.box');
- let activeLable = document.querySelector('.activeCircle').querySelectorAll('.fa-circle');
- let  nextBtn = document.querySelector('#nextBtn');
- let preBtn = document.querySelector('#preBtn');
+let wrapper = document.querySelector('.wrapper-box');
+let activeBox = wrapper.querySelectorAll('.box');
+let activeLable = document.querySelector('.activeCircle').querySelectorAll('.fa-circle');
+let nextBtn = document.querySelector('#nextBtn');
+let preBtn = document.querySelector('#preBtn');
 
- let indexNo = 0;
+let indexNo = 0;
 
 nextBtn.onclick = () =>{
-    indexNo++;
-    changeBox();
+   indexNo++;
+   changeBox();
 }
 
 preBtn.onclick = () =>{
-    indexNo--;
-    changeBox();
+   indexNo--;
+   changeBox();
 }
 
 let changeBox = () =>{
-    if(indexNo > activeBox.length-1){
-        indexNo=0;
-    } else if(indexNo < 0){
-        indexNo = activeBox.length -1;
+   if(indexNo > activeBox.length-1){
+       indexNo=0;
+   } else if(indexNo < 0){
+       indexNo = activeBox.length -1;
+   }
+
+   for(let i=0; i < activeBox.length; i++){
+       if(i === indexNo){
+           activeBox[i].classList.add('active');
+           activeLable[i].classList.add('fa-solid');
+
+           if(window.screen.width > 768){
+               wrapper.style.transform = `translateX(${indexNo* -250}px)`;
+           }
+
+           
+
+       }else{
+           activeBox[i].classList.remove('active');
+           activeLable[i].classList.remove('fa-solid');
+       }
+   }
+}
+
+ let fwrapper = document.querySelector('.featured-wrapper-box');
+ let fActBox = fwrapper.querySelectorAll('.box');
+ let fActLable = document.querySelector('.fActCircle').querySelectorAll('.fa-circle');
+ let fNextBtn = document.querySelector('#fNextBtn');
+ let fPreBtn = document.querySelector('#fPreBtn');
+
+ let fIndexNo = 0;
+
+fNextBtn.onclick = () =>{
+    fIndexNo++;
+    fChangeBox();
+}
+
+fPreBtn.onclick = () =>{
+    fIndexNo--;
+    fChangeBox();
+}
+
+let fChangeBox = () =>{
+    if(fIndexNo > fActBox.length-1){
+        fIndexNo=0;
+    } else if(fIndexNo < 0){
+        fIndexNo = fActBox.length -1;
     }
 
-    for(let i=0; i < activeBox.length; i++){
-        if(i === indexNo){
-            activeBox[i].classList.add('active');
-            activeLable[i].classList.add('fa-solid');
+    for(let i=0; i < fActBox.length; i++){
+        if(i === fIndexNo){
+            fActBox[i].classList.add('active');
+            fActLable[i].classList.add('fa-solid');
 
             if(window.screen.width > 768){
-                wrapper.style.transform = `translateX(${indexNo* -250}px)`;
+                fwrapper.style.transform = `translateX(${fIndexNo* -21}vw)`;
             }
 
             
 
         }else{
-            activeBox[i].classList.remove('active');
-            activeLable[i].classList.remove('fa-solid');
+            fActBox[i].classList.remove('active');
+            fActLable[i].classList.remove('fa-solid');
+        }
+    }
+}
+
+
+let rwrapper = document.querySelector('.review-wrapper-box');
+ let rActBox = rwrapper.querySelectorAll('.box');
+ let rActLable = document.querySelector('.rActCircle').querySelectorAll('.fa-circle');
+ let rNextBtn = document.querySelector('#rNextBtn');
+ let rPreBtn = document.querySelector('#rPreBtn');
+
+ let rIndexNo = 0;
+
+rNextBtn.onclick = () =>{
+    rIndexNo++;
+    rChangeBox();
+}
+
+rPreBtn.onclick = () =>{
+    rIndexNo--;
+    rChangeBox();
+}
+
+let rChangeBox = () =>{
+    if(rIndexNo > rActBox.length-1){
+        rIndexNo=0;
+    } else if(rIndexNo < 0){
+        rIndexNo = rActBox.length -1;
+    }
+
+    for(let i=0; i < rActBox.length; i++){
+        if(i === rIndexNo){
+            rActBox[i].classList.add('active');
+            rActLable[i].classList.add('fa-solid');
+
+            if(window.screen.width > 768){
+                rwrapper.style.transform = `translateX(${rIndexNo* -20}vw)`;
+            }
+
+            
+
+        }else{
+            rActBox[i].classList.remove('active');
+            rActLable[i].classList.remove('fa-solid');
         }
     }
 }
